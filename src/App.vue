b901344e67d2a369eba79d9f524201ed17b6bc51
Noa Gobert <nonocheclub@gmail.com>
2023-11-19 15:39:00 +0100

    start de la new interface

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,24 @@
 <template>
-    <div class="bg-gray-200 min-h-screen flex items-center justify-center">
-        <div class="bg-white rounded-lg shadow-lg p-8">
-            <h1 class="text-2xl font-bold mb-4">Search for a city</h1>
-            <input
-                type="text"
-                class="w-full bg-gray-200 bg-opacity-50 rounded-lg p-4 focus:outline-none focus:bg-opacity-100"
-                placeholder="Enter city name"
-                v-model="city"
-                @keyup.enter="fetchWeather"
-            />
-            <div v-if="loading" class="mt-4 text-xl font-bold">Loading...</div>
-            <div v-if="error" class="mt-4 text-xl font-bold text-red-500">
-                {{ error }}
-            </div>
-            <div v-if="weather" class="mt-4">
-                <h2 class="text-3xl font-bold">
-                    {{ weather.name }}, {{ weather.sys.country }}
-                </h2>
-                <div class="flex items-center mt-4">
-                    <img
-                        :src="
-                            'https://openweathermap.org/img/wn/' +
-                            weather.weather[0].icon +
-                            '@2x.png'
-                        "
-                        :alt="weather.weather[0].description"
-                        class="w-16 h-16 mr-4"
+    <div class="bg-white min-h-screen flex items-center justify-center">
+        <div class="bg-primary w-[80%] rounded-lg shadow-lg p-8">
+            <div class="flex items-center justify-between">
+                <div
+                    class="flex justify-center items-center gap-2 bg-secondary rounded-lg p-2"
+                >
+                    <font-awesome-icon
+                        icon="magnifying-glass"
+                        style="color: #ffffff"
                     />
-                    <p class="text-xl">{{ weather.weather[0].description }}</p>
+                    <input
+                        type="text"
+                        placeholder="Search for a city..."
+                        class="placeholder-white bg-transparent border-none focus:outline-none text-white text-lg"
+                    />
                 </div>
-                <p class="text-6xl font-bold mt-4">
-                    {{ Math.round(weather.main.temp) }} &#8451;
-                </p>
 
-                <div class="mt-8 flex justify-between">
-                    <div>
-                        <p class="text-lg font-bold mb-2">Wind Speed</p>
-                        <p
-                            class="text-3xl font-bold text-green-500 leading-none"
-                        >
-                            {{ Math.round(weather.wind.speed) }} km/h
-                        </p>
-                    </div>
-                    <div>
-                        <p class="text-lg font-bold mb-2">Humidity</p>
-                        <p
-                            class="text-3xl font-bold text-blue-500 leading-none"
-                        >
-                            {{ weather.main.humidity }} %
-                        </p>
-                    </div>
+                <div class="text-white text-3xl flex items-center gap-3">
+                    <button>&deg;C</button>
+                    <button>F&deg;</button>
                 </div>
             </div>
         </div>
@@ -84,7 +53,6 @@
             }
             weather.value = data;
         } catch (err) {
-            console.error(err);
             error.value = "There was a problem fetching the weather data.";
         } finally {
             loading.value = false;
@@ -92,17 +60,10 @@
     };
 
     const fetchWeatherByLocation = async () => {
-        console.log("call");
-
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(async position => {
-                console.log(
-                    position.coords.latitude,
-                    position.coords.longitude
-                );
                 latitude.value = position.coords.latitude;
                 longitude.value = position.coords.longitude;
-                console.log(latitude.value, longitude.value);
 
                 const url = `${API_URL}?lat=${latitude.value}&lon=${longitude.value}&appid=${apiKey}&units=metric`;
 
@@ -114,7 +75,6 @@
                     }
                     weather.value = data;
                 } catch (err) {
-                    console.error(err);
                     error.value =
                         "There was a problem fetching the weather data.";
                 } finally {
--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './style.css'
+
 import { library } from '@fortawesome/fontawesome-svg-core'
 
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 
-/* add icons to the library */
-library.add(faUserSecret)
-createApp(App).mount('#app')
+library.add(faMagnifyingGlass)
+
+createApp(App)
+    .component('font-awesome-icon', FontAwesomeIcon)
+    .mount('#app')
